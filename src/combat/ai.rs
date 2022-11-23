939ca60ab5665713c39aa3917e4918e0171dd860
Othi <mnpq.raven@gmail.com>
2022-11-24 00:03:41 +0700

    refactoring targeting filter

--- src/combat/ai.rs
+++ src/combat/ai.rs
@@ -27,6 +27,7 @@ fn choose_skill(
     mut ev_castskill: EventWriter<CastSkillEvent>,
     mut ev_choose_ai_skill: EventReader<ChooseAISkillEvent>,
     mut commands: Commands,
+    current_caster: Res<CurrentCaster>,
 ) {
     for _ in ev_choose_ai_skill.iter() {
         if !&turnorder.is_empty() {
@@ -38,38 +39,58 @@ fn choose_skill(
                 true if enemy_tag.is_some() => SkillGroup::Enemy,
                 _ => SkillGroup::Universal,
             };
+            // gather all skills from either enemy grp or ally
             let pool: Vec<(Entity, &SkillGroup, &Target)> =
                 skill_q.iter().filter(|item| item.1.eq(&filter)).collect();
             let rng_index = thread_rng().gen_range(0..pool.len());
+            // TODO: refactor with ui/combat.rs
+            // TODO: Self should not be on player
+            // arguments:
+            // &Target
+            // Query with Entity and unit tags
+            // CurrentCaster resource
+            let target_type = pool[rng_index].2;
+            let filtered_units = target_search_q.iter().filter(
+                |(e, .., player_tag, ally_tag, enemy_tag)| match target_type {
+                    Target::Player => player_tag.is_some(),
+                    Target::AllyAndSelf | Target::AllyAOE => player_tag.is_some() || ally_tag.is_some(),
+                    Target::AllyButSelf => player_tag.is_none() && ally_tag.is_some(),
+                    Target::Enemy | Target::EnemyAOE => enemy_tag.is_some(),
+                    Target::Any => true,
+                    Target::AnyButSelf => player_tag.is_none(),
+                    Target::NoneButSelf => e == &current_caster.0.unwrap()
+                });
             // hard code target selecting
-            let target_list: Vec<(Entity, Option<&Player>, Option<&Ally>, Option<&Enemy>)> =
-                // TODO: refactor this monstrosity
-                match pool[rng_index].2 {
-                    Target::Player | Target::NoneButSelf => target_search_q
-                        .iter()
-                        .filter(|item| item.1.is_some())
-                        .collect(),
-                    Target::AllyAndSelf | Target::AllyAOE => target_search_q
-                        .iter()
-                        .filter(|item| item.2.is_some() || item.1.is_some())
-                        .collect(),
-                    Target::AllyButSelf => target_search_q
-                        .iter()
-                        .filter(|item| item.2.is_some() && item.1.is_none())
-                        .collect(),
-                    Target::Enemy | Target::EnemyAOE => target_search_q
-                        .iter()
-                        .filter(|item| item.3.is_some())
-                        .collect(),
-                    Target::Any => target_search_q
-                        .iter()
-                        .filter(|item| item.1.is_some() || item.2.is_some() || item.3.is_some())
-                        .collect(),
-                    Target::AnyButSelf => target_search_q
-                        .iter()
-                        .filter(|item| item.1.is_none())
-                        .collect(),
-                };
+            // let target_list: Vec<(Entity, Option<&Player>, Option<&Ally>, Option<&Enemy>)> =
+            //     // TODO: refactor this monstrosity
+            //     match pool[rng_index].2 {
+            //         Target::Player | Target::NoneButSelf => target_search_q
+            //             .iter()
+            //             .filter(|item| item.1.is_some())
+            //             .collect(),
+            //         Target::AllyAndSelf | Target::AllyAOE => target_search_q
+            //             .iter()
+            //             .filter(|item| item.2.is_some() || item.1.is_some())
+            //             .collect(),
+            //         Target::AllyButSelf => target_search_q
+            //             .iter()
+            //             .filter(|item| item.2.is_some() && item.1.is_none())
+            //             .collect(),
+            //         Target::Enemy | Target::EnemyAOE => target_search_q
+            //             .iter()
+            //             .filter(|item| item.3.is_some())
+            //             .collect(),
+            //         Target::Any => target_search_q
+            //             .iter()
+            //             .filter(|item| item.1.is_some() || item.2.is_some() || item.3.is_some())
+            //             .collect(),
+            //         Target::AnyButSelf => target_search_q
+            //             .iter()
+            //             .filter(|item| item.1.is_none())
+            //             .collect(),
+            //     };
+            let target_list: Vec<(Entity, Option<&Player>, Option<&Ally>, Option<&Enemy>)>
+                = filtered_units.collect();
             let target_ent: Entity = target_list[0].0;
             commands.insert_resource(CurrentCaster(Some(unit_ent)));
             ev_castskill.send(CastSkillEvent {
